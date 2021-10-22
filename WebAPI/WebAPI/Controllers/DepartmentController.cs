e10883152fd8f6091b256ee0566d3ae4e774447d
Steve Russell <steve.russell@swintontechnology.com>
2021-10-22 13:07:52 +0100

    Added handlers for updating and deleting a department

--- WebAPI/WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -44,9 +44,10 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost]
+        //add a new deportment by POSTing {"DepartmentName":"Accounts"}
         public JsonResult Post(Department department)
         {
-            string query = @"insert into dbo.Department values ('"+department.DepartmentName+@"')";
+            string query = @"insert into dbo.Department values ('" + department.DepartmentName + @"')";
 
             DataTable table = new DataTable();
             string sqlDataSource = _config.GetConnectionString("EmployeeAppCon");
@@ -66,6 +67,62 @@ namespace WebAPI.Controllers
             }
 
             return new JsonResult("Added " + department.DepartmentName + " Successfully");
+        }
+        [HttpPut]
+        //update a deportment by PUIting {"DepartmentId":3,"DepartmentName":"Accounts"}
+        public JsonResult Put(Department department)
+        {
+            string query = @"
+                            update dbo.Department set
+                                DepartmentName='" + department.DepartmentName + @"'
+                                where DepartmentId="+department.DepartmentId;
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppCon");
+
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCmd = new SqlCommand(query, myCon))
+                {
+                    myReader = myCmd.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated " + department.DepartmentName + " Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        //example DELETE from Postman : http://localhost:14763/api/department/3
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Department 
+                                where DepartmentId=" + id;
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("EmployeeAppCon");
+
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCmd = new SqlCommand(query, myCon))
+                {
+                    myReader = myCmd.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted department " + id);
         }
     }
 }
