a19d34be4f6372ededf1f8c3e5e5063806b1551b
Steve Russell <steve.russell@swintontechnology.com>
2021-10-28 12:55:43 +0100

    Basic navigation structure added with landing pages for home, dept and employee. It needs package react-router-dom installing 'npm install --save react-router-dom'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+// import the models we created
+import {Home} from './Home'
+import {Employee} from './Employee'
+import {Department} from './Department'
+import {Navigation} from './Navigation'
+
+// From react router
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
       <h3 className="m-3 d-flex justify-content-center">
-        React Training
+        React UI
       </h3>
+      <Navigation/>
+      <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/department' component={Department} exact/>
+        <Route path='/employee' component={Employee} exact/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
