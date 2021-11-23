10feb59c898afa40ba591c44891cf328ebab629c
Steve Russell <steve.russell@swintontechnology.com>
2021-11-23 12:49:32 +0000

    Added the ability to add a department

--- my-app/src/Department.js
+++ my-app/src/Department.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import {Table} from 'react-bootstrap';
 
+import {Button, ButtonToolbar} from 'react-bootstrap';
+import {AddDepModal} from './AddDepModal';
+
 export class Department extends Component {
     
     // store variables in the state. Department data will be stored here.
     constructor(props) {
         super(props);
-        this.state = {deps:[]}
+        this.state = {deps:[], addModalShow:false, editModalShow:false}
         console.log("const");
     }
 
     refreshList() {
-        fetch("http://localhost:14763/api/department")      //process.env.REACT_APP_API+"department")
+        console.log("refresh"+process.env.REACT_APP_API);
+        fetch("http://localhost:14763/api/department")       //process.env.REACT_APP_API+"department")
         .then(response=>response.json())
         .then(data=>{   // data is available so save it in the state
             this.setState({deps:data});
@@ -34,24 +38,35 @@ export class Department extends Component {
     }
 
     render() {
-        const {deps}=this.state; // use bootstrap to display the data with additional edit/delete column
+        const {deps, depid, depname}=this.state; // use bootstrap to display the data with additional edit/delete column
+        let addModalClose=()=>this.setState({addModalShow:false});
+        let editModalClose=()=>this.setState({editModalShow:false});
         return(
             <div>
                 <Table className="mt-4" striped bordered hover size="sm">
                     <thead>
-                        <tr>DepartmentId</tr>
-                        <tr>DepartmentName</tr>
-                        <tr>Modify</tr> 
+                        <tr>
+                            <th>DepartmentId</th>
+                            <th>DepartmentName</th>
+                            <th>Modify</th>
+                        </tr> 
                     </thead>
                     <tbody>
                         {deps.map(dep=>
                         <tr key={dep.DepartmentId}>
                             <td>{dep.DepartmentId}</td>
                             <td>{dep.DepartmentName}</td>
-                            <td>Edit / Delete</td>
+                            <td>Edit/Del
+                            </td>
                         </tr>)}
                     </tbody>
                 </Table>
+                <ButtonToolbar>
+                    <Button variant='primary' onClick={()=>this.setState({addModalShow:true})}>
+                        Add Department
+                    </Button>
+                    <AddDepModal show={this.state.addModalShow} onHide={addModalClose}/>
+                </ButtonToolbar>
             </div>
         )
     }
